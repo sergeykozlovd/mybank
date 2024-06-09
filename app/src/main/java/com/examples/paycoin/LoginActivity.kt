17e5b951786d6cbb8410e141fdabec3c1e7b7b2a
Sergey <sergeykozlov.d@gmail.com>
2024-06-09 13:18:35 +0500

    next

--- app/src/main/java/com/examples/paycoin/LoginActivity.kt
+++ app/src/main/java/com/examples/paycoin/LoginActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity:AppCompatActivity(R.layout.activity_login) {
 
 
     private lateinit var enterButton:MaterialButton
+    private lateinit var etLogin: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initViews()
@@ -17,6 +19,9 @@ class LoginActivity:AppCompatActivity(R.layout.activity_login) {
 
     private fun initViews(){
         enterButton = findViewById(R.id.enterButton)
+        etLogin = findViewById(R.id.etLogin)
+
+        etLogin.setText( App.prefs.getString(KEY_LOGIN,""))
     }
 
     private fun initListeners(){
