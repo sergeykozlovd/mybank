3cfb1c19f1339cb42498016915a1fda2ae91f607
Sergey <sergeykozlov.d@gmail.com>
2024-06-10 08:22:01 +0500

    finish

--- app/src/main/java/com/examples/paycoin/PaymentActivity.kt
+++ app/src/main/java/com/examples/paycoin/PaymentActivity.kt
@@ -1,35 +1,35 @@
 package com.examples.paycoin
 
 import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
 import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import android.media.RingtoneManager
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 
+enum class SoundType{
+    SUCCESS,
+    FAILURE,
+    SCAN_FINISH
+}
+class PaymentActivity : AppCompatActivity(R.layout.activity_payment) {
 
-class PaymentActivity:AppCompatActivity(R.layout.activity_payment) {
-
-//    private  lateinit var ivCardAnim:ImageView
-    private  lateinit var toolbar: Toolbar
-    private  lateinit var tvAmount: TextView
-    private lateinit var textConnecting:TextView
+    private var mediaPlayer: MediaPlayer? = null
+    private lateinit var toolbar: Toolbar
+    private lateinit var tvAmount: TextView
+    private lateinit var textStatus: TextView
     private var amount = 0
     private var timeout = 0
     private var messageDialog = ""
     private var isSuccess = true
 
-    private var isPlayAnimate = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getParam()
@@ -39,10 +39,28 @@ class PaymentActivity:AppCompatActivity(R.layout.activity_payment) {
         paymentDone()
     }
 
-    private fun initAnimation(){
+    private fun playSound(soundType: SoundType){
+            mediaPlayer = MediaPlayer.create(
+                this,
+                when (soundType){
+                    SoundType.SUCCESS -> R.raw.success
+                    SoundType.FAILURE -> R.raw.failure
+                    SoundType.SCAN_FINISH -> R.raw.click
+                }
+            )
+            mediaPlayer?.start()
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mediaPlayer != null) {
+            mediaPlayer?.release()
+            mediaPlayer = null
+        }
+    }
 
-        ObjectAnimator.ofFloat(textConnecting, "alpha",0f, 1f).apply {
+    private fun initAnimation() {
+        ObjectAnimator.ofFloat(textStatus, "alpha", 0f, 1f).apply {
             duration = 700
             repeatCount = ObjectAnimator.INFINITE
             repeatMode = ObjectAnimator.REVERSE
@@ -50,16 +68,20 @@ class PaymentActivity:AppCompatActivity(R.layout.activity_payment) {
     }
 
 
-    private fun paymentDone(){
+    private fun paymentDone() {
         Handler(Looper.getMainLooper()).postDelayed({
-            isPlayAnimate = false
             if (!isFinishing) {
-                showDoneDialog()
+                textStatus.text = getString(R.string.connecting)
+                playSound(SoundType.SCAN_FINISH)
+                Handler(Looper.getMainLooper()).postDelayed({
+                    showDoneDialog()
+                    playSound(if (isSuccess) SoundType.SUCCESS else SoundType.FAILURE )
+                }, 2000)
             }
-        },(timeout * 1000).toLong())
+        }, (timeout * 1000).toLong())
     }
 
-    private fun showDoneDialog(){
+    private fun showDoneDialog() {
         val dialog = Dialog(this)
         dialog.setContentView(R.layout.dialog_payment_done)
         dialog.setCanceledOnTouchOutside(false)
@@ -69,41 +91,32 @@ class PaymentActivity:AppCompatActivity(R.layout.activity_payment) {
             Toolbar.LayoutParams.WRAP_CONTENT
         )
         dialog.findViewById<TextView>(R.id.tvMessageDialog).setText(messageDialog)
-        dialog.findViewById<ImageView>(R.id.imageView).setImageResource( if (isSuccess) R.drawable.ic_payment_success else R.drawable.ic_payment_failure)
+        dialog.findViewById<ImageView>(R.id.imageView)
+            .setImageResource(if (isSuccess) R.drawable.ic_payment_success else R.drawable.ic_payment_failure)
         dialog.findViewById<Button>(R.id.button).setOnClickListener {
             dialog.dismiss()
             finish()
         }
         dialog.show()
-        try {
-
-            RingtoneManager.getRingtone(
-                applicationContext,
-                RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-            ).play()
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
     }
 
-    private fun getParam(){
-        amount = intent.getIntExtra(KEY_AMOUNT,0)
-        timeout = intent.getIntExtra(KEY_PAY_TIMEOUT,0)
+    private fun getParam() {
+        amount = intent.getIntExtra(KEY_AMOUNT, 0)
+        timeout = intent.getIntExtra(KEY_PAY_TIMEOUT, 0)
         messageDialog = intent.getStringExtra(KEY_MESSAGE_DIALOG).toString()
-        isSuccess = intent.getBooleanExtra(KEY_IS_SUCCESS,true)
+        isSuccess = intent.getBooleanExtra(KEY_IS_SUCCESS, true)
     }
 
-    private fun initListeners(){
+    private fun initListeners() {
         toolbar.setNavigationOnClickListener {
             finish()
         }
     }
 
-    private fun  initViews(){
-        textConnecting = findViewById(R.id.textConnection)
+    private fun initViews() {
         toolbar = findViewById(R.id.toolbar)
         tvAmount = findViewById(R.id.tvAmount)
+        textStatus = findViewById(R.id.textStatus)
         tvAmount.text = "Pay Amount ${amount}"
     }
 
